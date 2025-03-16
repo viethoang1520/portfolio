82c5383c7e64ed4f90c378c97fe2aca1d590c621
viethoang1520 <viethoang1520.dev@gmail.com>
2025-03-16 12:20:37 +0700

    modify navbar selection states

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,23 +4,26 @@ import('./Header.scss')
 import { useDispatch } from 'react-redux';
 import { toggle } from '@/store/theme/themeSlice';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 function Header() {
   const dispatch = useDispatch();
   const handleToggle = () => {
     dispatch(toggle());
   };
   const theme = useSelector((state) => state.theme.theme);
-  console.log(theme);
+
+  const [navbarSelected, setNavbarSelected] = useState('Home');
+
   return (
     <div className="header">
       <div className="logo-block">
        Portfolio.
       </div>
       <ul className="navbar-list">
-        <a href='#introduction'><li className='navbar-items'>Home</li></a>
-        <a href='#skill'><li className='navbar-items'>Skills</li></a>
-        <a href='#projects'><li className='navbar-items'>Projects</li></a>
-        <a href='#contact'><li className='navbar-items'>Contact</li></a>
+        <a href='#introduction'><li className={`navbar-items ${navbarSelected === 'Home' ? 'active' : ''}`} onClick={() => setNavbarSelected('Home')}>Home</li></a>
+        <a href='#skill'><li className={`navbar-items ${navbarSelected === 'Skills' ? 'active' : ''}`} onClick={() => setNavbarSelected('Skills')}>Skills</li></a>
+        <a href='#projects'><li className={`navbar-items ${navbarSelected === 'Projects' ? 'active' : ''}`} onClick={() => setNavbarSelected('Projects')}>Projects</li></a>
+        <a href='#contact'><li className={`navbar-items ${navbarSelected === 'Contact' ? 'active' : ''}`} onClick={() => setNavbarSelected('Contact')}>Contact</li></a>
       </ul>
       <div onClick={handleToggle} className="right-block">
         {theme === 'light-theme' ?(
