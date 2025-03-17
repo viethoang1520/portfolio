51664dc3705a95c9ef2e3cd7a300d8a2b5915e3a
viethoang1520 <viethoang1520.dev@gmail.com>
2025-03-18 01:34:27 +0700

    delete: previous project card

--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -5,25 +5,30 @@ import { Link } from 'react-router-dom'
 
 function ProjectCard({ project }) {
   return (
-    <div className="project-card">
-      <h1 className="name">{project.name}</h1>
-      <p className="description">{project.description}</p>
-      <ul className="tech-list">
-        {project.techs.map((tech, index) => (
-          <li key={index} className="tech-item">{tech}</li>
-        ))}
-      </ul>
-      <div className="icon-block">
-        <Link className='link-icon github-icon'>
-          <Icon icon="line-md:github" className='icon github-icon' />
-          <p>GitHub</p>
-        </Link>
-        <Link className='link-icon live-demo'>
-          <Icon icon="eva:external-link-fill" className='icon ex-link' />
-          <p>Live Demo</p>
-        </Link>
+    // <div className="project-card">
+    //   <h1 className="name">{project.name}</h1>
+    //   <p className="description">{project.description}</p>
+    //   <ul className="tech-list">
+    //     {project.techs.map((tech, index) => (
+    //       <li key={index} className="tech-item">{tech}</li>
+    //     ))}
+    //   </ul>
+    //   <div className="icon-block">
+    //     <Link className='link-icon github-icon'>
+    //       <Icon icon="line-md:github" className='icon github-icon' />
+    //       <p>GitHub</p>
+    //     </Link>
+    //     <Link className='link-icon live-demo'>
+    //       <Icon icon="eva:external-link-fill" className='icon ex-link' />
+    //       <p>Live Demo</p>
+    //     </Link>
+    //   </div>
+    // </div >
+    <div className='project-card-container'>
+      <div className="project-card">
+        
       </div>
-    </div >
+    </div>
   );
 }
 
