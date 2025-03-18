07b215747557f85e168bf01977679b2f7b826f4c
viethoang1520 <viethoang1520.dev@gmail.com>
2025-03-18 11:54:00 +0700

    release: standard animation, styling for titles

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -10,7 +10,10 @@ export default function Contact() {
   return (
     <div className="contact-block">
       <div className='contact container'>
-        <h1 className='contact-title'><p>CONTACT</p> <p className='title-color'>ME!</p></h1>
+        <div className="contact-title-container">
+          <h1 data-aos="zoom-in-up" className='contact-title'><p>CONTACT</p> <p className='title-color'>ME!</p></h1>
+          <div data-aos="zoom-in-up" data-aos-delay="100" className="contact-title-line"></div>
+        </div>
         <Row gutter={[24, 14]} className='input-block'>
           <Col xs={24} sm={24} md={12}>
             <Input size='large' className='input-element' placeholder="Full Name" />
