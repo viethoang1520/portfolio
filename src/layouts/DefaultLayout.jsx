bb732cb900043679c22d46561dc8340ef9415eb2
viethoang1520 <viethoang1520.dev@gmail.com>
2025-03-25 23:09:37 +0700

    feat: layout imports

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import { Row, Col } from 'antd'
 export default function DefaultLayout() {
   return (
     <div>
-      
+      <Row>
+        <Col>
+        </Col>
+        <Col>
+        </Col>
+      </Row>
     </div>
   )
 }
