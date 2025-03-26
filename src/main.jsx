2df1f8e43eb95ad7494f68a34e7baccaf2aa1974
viethoang1520 <viethoang1520.dev@gmail.com>
2025-03-26 09:14:44 +0700

    feat: style folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import GlobalStyles from './components/GlobalStyles/GlobalStyles.jsx'
+import GlobalStyles from '@/styles/GlobalStyles.jsx'
 import { Provider } from 'react-redux'
 import { store } from '@/store/index.js'
 
