bedd6e5427f9444174e5d42ede28c69873dd05ba
viethoang1520 <viethoang1520.dev@gmail.com>
2025-03-27 21:56:37 +0700

    chore: introduction styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -18,11 +18,11 @@ function Hero() {
         </Col>
         <Col data-aos="fade-up" data-aos-delay="200" sm={24} md={24} lg={12} xl={12} className="introduction">
           <div className="title-block">
-            <h1 className='content upper-title'>Hi there👋, I'm Viet Hoang</h1>
-            <div className="lower-title">
-              <h1 className='content style'>Back-end</h1>
-              <h1 className='content lower-title'> Developer</h1>
-            </div>
+            <h1 className='content upper-title'>Hi👋 I'm Viet Hoang</h1>
+            <h1 className="lower-title">
+              <p className='content style'>Back-end</p>
+              <p className='content lower-title'> Developer</p>
+            </h1>
           </div>
           <div className="description">
             <h5 className="desc-content">"Welcome to my portfolio! My name is Viet Hoang, and I am currently a student at FPT University, where I am dedicated to expanding my knowledge and skills in various fields. I am always eager to learn and embrace new challenges, as I believe that personal growth comes from stepping outside of my comfort zone."</h5>
